6d17da2254d6657b7380e6e3df878990f5c7bb59
Berserk883 <egorhyldikob571@gmail.com>
2025-04-27 15:33:30 +0800

    yea

---
+++ js/tabs.js
@@ -0,0 +1,15 @@
+var lastTab = document.getElementById('firstActiveTab')
+var lastTabContent = document.getElementById(lastTab.getAttribute('data-tab-id'))
+
+function clickTab(event){
+    const target = event.target;
+    const targetContent = document.getElementById(target.getAttribute('data-tab-id'))
+    
+    lastTabContent.classList.add('invisible')
+    targetContent.classList.remove('invisible')
+    lastTabContent = targetContent
+
+    lastTab.classList.remove('tab_active')
+    target.classList.add('tab_active')
+    lastTab = target
+}
